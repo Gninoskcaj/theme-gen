8183eda2694320059d126be2b5421d5f56ed06e0
Jackson Mooring <jacksonmooring@Jacksons-Mac-mini.local>
2019-08-15 09:00:36 -0500

    Create the file

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 module.exports = () => {
+    let data = '/*Hey There */'
+    fs.writeFileSync('theme.css', data)
     console.log(chalk.green('Done!'))
 }
